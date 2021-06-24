f5dedc19c4e54ac1e9a1a32002edf3ad4e1a1104
Yves Seurynck <yves.seurynck@vives.be>
2021-06-24 11:42:59 +0200

    first commit

---
+++ src/test/java/be/vives/ti/AppTest.java
@@ -0,0 +1,13 @@
+package be.vives.ti;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.assertTrue;
+
+public class AppTest {
+
+  @Test
+  void stupidTest() {
+    assertTrue(true);
+  }
+}
